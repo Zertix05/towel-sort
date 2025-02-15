253c296892411843501230396672416dbbaa8331
Zertix <3t5i4m8a.bel@gmail.com>
2025-02-15 21:40:02 +0300

    first project on Node.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix)
+    return[];
+
+  var arr2 = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if(i%2===0){
+        arr2.push(...matrix[i]);
+    }
+    else{
+        arr2.push(...matrix[i].slice().reverse());
+    }
+    }
+    return arr2;
+
 }
